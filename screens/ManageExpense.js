3090e80ce7e2ab21cac361d653d4eb2e2a3f77d7
alenslyn <alenslyn@gmail.com>
2024-05-06 16:58:13 +0200

    feat: add screen title change

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,16 @@
 import { Text } from "react-native";
+import { useLayoutEffect } from "react";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEdititng = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEdititng ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEdititng]);
+
   return <Text> Manage Expense screen</Text>;
 }
 
