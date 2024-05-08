55157d65d51430b4761fa070ccc85c926f27a027
alenslyn <alenslyn@gmail.com>
2024-05-08 16:28:13 +0200

    feat: add input fields

--- components/manageExpense/ExpenseForm.js
+++ components/manageExpense/ExpenseForm.js
@@ -1,7 +1,35 @@
 import { View, TextInput, StyleSheet } from "react-native";
+import Input from "./Input";
 
 function ExpenseForm() {
-  return <View></View>;
+  function amountChangedHandler() {}
+  return (
+    <View>
+      <Input
+        label="Amount"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: amountChangedHandler,
+        }}
+      />
+      <Input
+        label="Date"
+        textInputConfig={{
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          autoCorrect: false, //def is true
+          //   autoCapitalize: none
+        }}
+      />
+    </View>
+  );
 }
 
 export default ExpenseForm;
